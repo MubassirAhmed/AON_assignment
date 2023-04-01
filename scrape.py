ae4b031b80b16a2f502d8d02a2a77f5fb09c40d5
MubassirAhmed <mubassirahmed01@gmail.com>
2023-04-01 17:51:23 -0400

    scrape, get pdf link - up

--- scrape.py
+++ scrape.py
@@ -2,22 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-DIN = '02510839'
-product_name = 'VYEPTI'
-
-
 options = Options()
-options.headless = True
+#options.headless = True
 options.add_argument("--window-size=1920,1200")
-
 DRIVER_PATH = '.chromedriver'
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+
+####################################################
+####################################################
+
+
+#Page 1, entering DIN and product name
+DIN = '02510839'
+product_name = 'VYEPTI'
 
 driver.get("https://health-products.canada.ca/dpd-bdpp/")
 DIN_field = driver.find_element(By.ID, "din").send_keys(DIN)
 product_field = driver.find_element(By.ID, "product").send_keys(product_name)
 search = driver.find_element("xpath", "//input[@value='Search']").click()
 
-print(driver.page_source)
 
+#Page 2, choosing the first instance of result
+Click_ProductInfo_HyperLink = driver.find_element("link text", DIN).click()
+
+#Page 3, getting the pdf link
+Product_Monograph_URL = driver.find_element(By.CLASS_NAME, 'glyphicon-paperclip').find_element(By.TAG_NAME, 'a').get_attribute('href')
+print(Product_Monograph_URL)
 driver.quit()
