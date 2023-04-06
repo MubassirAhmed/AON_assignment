ab022a99cba75af210e371632b233d7bf8b721ca
MubassirAhmed <mubassirahmed01@gmail.com>
2023-04-06 13:19:20 -0400

    documented code - up

--- scrape.py
+++ scrape.py
@@ -7,162 +7,169 @@ from webdriver_manager.chrome import ChromeDriverManager
 import requests
 import fitz
 from pdf2image import convert_from_path
-from pdf2jpg import pdf2jpg
 import pytesseract
-import os
-
-def get_ProductMonograph_download_URL(DIN,product_name):
-	options = Options()
-	options.add_argument("--headless=new")
-	options.add_argument("--window-size=1920,1200")
-	DRIVER_PATH = '.chromedriver'
-	driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
-
-	#Page 1, entering DIN and product name
-	driver.get("https://health-products.canada.ca/dpd-bdpp/")
-	#time.sleep(1)
-	DIN_field = driver.find_element(By.ID, "din").send_keys(DIN)
-	#time.sleep(1)
-	product_field = driver.find_element(By.ID, "product").send_keys(product_name)
-	#time.sleep(1)
-	search = driver.find_element("xpath", "//input[@value='Search']").click()
-	#time.sleep(1)
-	#Page 2, choosing the first result
-	try:
-		Click_ProductInfo_HyperLink = driver.find_element("link text", DIN).click()
-		#time.sleep(1)
-	#Page 3, getting the pdf link
-	except:
-		pass
-
-	finally:
-		Product_Monograph_URL = driver.find_element(By.CLASS_NAME, 'glyphicon-paperclip').find_element(By.TAG_NAME, 'a').get_attribute('href')
-		#time.sleep(1)
-		driver.quit()
-	return Product_Monograph_URL
-
-
-def input(PATH_TO_XLSX):
-	df = pd.read_excel(PATH_TO_XLSX)
-	df = df.rename(columns=lambda x: x.strip())  #Clean up whitespace column names
-	df = df.drop(columns = 'Drug Class')
-	df = df.dropna()
-	for col in df.columns:  #Clean up whitespace in rows
-		df[col] = df[col].astype(str).str.strip()
-	#fixing non-zero prefix in excel
-	for i in range(df.shape[0]):
-		if len(df.loc[i,'DIN']) < 8:
-			df.loc[i,'DIN'] = '0' + df.loc[i,'DIN']
-	return df
-
-
-def get_pdf_urls(df):
-	for row in range(df.shape[0]):
-		try:
-			df.loc[row, r'PDF Link'] = get_ProductMonograph_download_URL(
-											DIN = df.loc[row,'DIN'],
-											product_name = df.loc[row,'Brand Name']
-											)
-			print('SUCCESS: collected pdf URL for ' + df.loc[row,'DIN'] + ' and record number ' + str(row))
-		except:
-			print('FAIL: could not collect pdf URL for ' + df.loc[row,'DIN'] + ' and record number ' + str(row))
-
-	# Create txt file with links
-	with open('pdf_links.csv', 'w') as f:
-		f.write('DIN,Brand Name,PDF Link\n')
-		for i in range(df.shape[0]):
-			f.write(f"{df.loc[i,'DIN']},{df.loc[i,'Brand Name']},{df.loc[i,'PDF Link']}\n")
+import os, sys
 
 
 
-def download_pdfs():
-	df = pd.read_csv('pdf_links.csv', dtype = str)
-	for i in range(df.shape[0]):
-		try:
-			download(url = df.loc[i,'PDF Link'], file_name = df.loc[i,'DIN'])
-			print('SUCCESS: downloaded product monograph for ' + df.loc[i,'DIN'] + ' and record number ' + str(i))
-		except:
-			print('FAIL: couldn\'t download product monograph for ' + df.loc[i,'DIN'] + ' and record number ' + str(i))
+def scrape_health_canada(DIN,product_name):
+    # Setting up Selenium config
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--window-size=1920,1200")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+
+    #Page 1, entering DIN and product name
+    driver.get("https://health-products.canada.ca/dpd-bdpp/")
+    #time.sleep(1)
+    DIN_field = driver.find_element(By.ID, "din").send_keys(DIN)
+    #time.sleep(1)
+    product_field = driver.find_element(By.ID, "product").send_keys(product_name)
+    #time.sleep(1)
+    search = driver.find_element("xpath", "//input[@value='Search']").click()
+    #time.sleep(1)
+    
+    # If redirected to Page 2, choosing the first result
+    try:
+        Click_ProductInfo_HyperLink = driver.find_element("link text", DIN).click()
+        #time.sleep(1)
+    
+    # Page 3, getting the pdf link
+    except:
+        pass
+
+    finally:
+        Product_Monograph_URL = driver.find_element(By.CLASS_NAME, 'glyphicon-paperclip').find_element(By.TAG_NAME, 'a').get_attribute('href')
+        #time.sleep(1)
+        driver.quit()
+    return Product_Monograph_URL
+
+
+
+def cleanExcelfile(PATH_TO_XLSX):
+    df = pd.read_excel(PATH_TO_XLSX)
+    df = df.rename(columns=lambda x: x.strip())  # Clean up whitespace column names
+    try:
+        df = df.drop(columns = 'Drug Class')
+    except:
+        pass
+    df = df.dropna()  # Drop rows with empty values
+    for col in df.columns:  # Clean up whitespace in rows
+        df[col] = df[col].astype(str).str.strip()
+    # Fixing non-zero prefix in excel
+    for i in range(df.shape[0]):
+        if len(df.loc[i,'DIN']) < 8:
+            df.loc[i,'DIN'] = '0' + df.loc[i,'DIN']
+    return df
+
+
+
+def scrape_productMonographs_URLs(df):
+    # Calls the actual scraper 
+    for row in range(df.shape[0]):
+        try:
+            df.loc[row, r'PDF Link'] = scrape_health_canada(DIN = df.loc[row,'DIN'], product_name = df.loc[row,'Brand Name'])
+            print('SUCCESS: collected pdf URL for ' + df.loc[row,'DIN'] + ' and record number ' + str(row))
+        except:
+            print('FAIL: could not collect pdf URL for ' + df.loc[row,'DIN'] + ' and record number ' + str(row))
+    # Creates txt file with product monograph URLS (i.e. download links)
+    with open('pdf_links.csv', 'w') as f:
+        f.write('DIN,Brand Name,PDF Link\n')
+        for i in range(df.shape[0]):
+            f.write(f"{df.loc[i,'DIN']},{df.loc[i,'Brand Name']},{df.loc[i,'PDF Link']}\n")
+
+
+
+def download_productMonographs():
+    df = pd.read_csv('pdf_links.csv', dtype = str)  # Read txt file with product monograph URLS (i.e. download links)
+    # Call the actual downloader
+    for i in range(df.shape[0]):
+        try:
+            download(url = df.loc[i,'PDF Link'], file_name = df.loc[i,'DIN'])
+            print('SUCCESS: downloaded product monograph for ' + df.loc[i,'DIN'] + ' and record number ' + str(i))
+        except:
+            print('FAIL: couldn\'t download product monograph for ' + df.loc[i,'DIN'] + ' and record number ' + str(i))
+
 
 
 def download(url, file_name):
-	""" PDF will be dowloaded into same dir as main.py
-	"""
-	headers = {"User-Agent": "Chrome/51.0.2704.103",}
-	# Send GET request
-	response = requests.get(url, headers=headers)
-	# Save the PDF
-	try:
-		f = open(file_name + '.pdf', "wb")
-		f.write(response.content)
-		f.close()
-	except:
-		pass
-		#print(response.status_code)
+    headers = {"User-Agent": "Chrome/51.0.2704.103",}
+    response = requests.get(url, headers=headers)  # Send GET request
+    # Save the PDF
+    try:
+        f = open(file_name + '.pdf', "wb")
+        f.write(response.content)
+        f.close()
+    except:
+        print(response.status_code)
+    ### Note: PDF will be dowloaded into same dir as scrape.py
 
-
-def main():
-	pass
-# 	createListOfDINsAutomaticallyFromPDF()
-# 	get_list_of_DINs_and_productName()
-	# df = input(r'ListOfDINs.xlsx')
-	# print(df)
- 	#pdf_urls_list = get_pdf_urls(df)
-# 	createListOfPdfUrls()
-# 	download_pdfs_locally()
-# 	openPdfAndGrabDrugClassForEachPairOfDin()
-# 	returnXLSofDINsProductNameAndDrugClass()
 
 
 def pdftoimage(file_path):
-	doc = fitz.open(file_path)
-	doc.select([0])
-	doc.save('first_page-'+file_path)
-	#convert_from_path(pdf_path = 'first_page-'+file_path, output_folder = '.', thread_count = 4, fmt = 'jpg', output_file = file_path )
-	convert_from_path('first_page-'+file_path)[0].save(file_path.replace('.pdf','')+'.jpg', 'JPEG')
-	os.remove('first_page-' + file_path)
+    # Converts first page of product monograph pdf to img
+    doc = fitz.open(file_path)
+    doc.select([0])
+    doc.save('first_page-'+file_path)
+    convert_from_path('first_page-'+file_path)[0].save(file_path.replace('.pdf','')+'.jpg', 'JPEG') 
+    os.remove('first_page-' + file_path)
+    os.remove(file_path)
 
-def imagetoOCR(file_path):
-	pdf = pytesseract.image_to_pdf_or_hocr(file_path, extension='pdf')
-	with open('OCR-' + file_path.replace('.jpg','') + '.pdf' , 'w+b') as f:
-		f.write(pdf) # pdf type is bytes by default
-	os.remove(file_path)   
-	 
+
+
+def OCRimage(imagefile):
+    # Scans PDF's image with Google's OCR engine, to create a clean machine-readable first page
+    pdf = pytesseract.image_to_pdf_or_hocr(imagefile, extension='pdf')
+    with open('OCR-' + imagefile.replace('.jpg','') + '.pdf' , 'w+b') as f:
+        f.write(pdf)
+    os.remove(imagefile)   
+     
+     
+     
 def get_drug_class_PyMuPDF(file_path):
-	with fitz.open(file_path) as pdf:
-		first_page = list(pdf)[0]
-		all_blocks = first_page.get_text('blocks')
-		result = ''
-		# Identify the first left-aligned textblock and printing the textbox immediately before 
-		for i, block in enumerate(all_blocks):
-			# initialize
-			x1 = round(block[0])
-			block_index = block[5]
-			#print(str(x1) + ' - ' + block[4].strip())
+    # Opens first page of PDF 
+    with fitz.open(file_path) as pdf:
+        first_page = list(pdf)[0]
+        all_blocks = first_page.get_text('blocks')  # Extracts all textblocks
+        result = ''
+        
+        # Identify the textblock of the 'Drug Class'
+        ### Note: It is assumed that the drug class is the last line of text that is center-justified
+        for i, block in enumerate(all_blocks):
+            x1 = round(block[0])
+            block_index = block[5]
    
-			# 6 - 9 because this is where the drug class is usually found
-			if block_index >= 6 and block_index <= 9:
-				if x1 >= 100 and x1 <= 300:
-					print(all_blocks[i-1][4].strip())
-					result = all_blocks[i-1][4].strip()
-					break
-		os.remove(file_path)
-		return result
-	
-   		# Notes:
-		# (x0, y0, x1, y1, 'lines of the block', block_no, block_type)
-	 
-	 
+            # Check lines 6 to 9 because this is where the drug class is usually found
+            if block_index >= 6 and block_index <= 9:
+                if x1 >= 100 and x1 <= 300:   # textblock is left-aligned if x1 coordinate is in the 100s or 200s
+                    print('Extracted: ' + all_blocks[i-1][4].strip())
+                    result = all_blocks[i-1][4].strip()   # Collect the textblock that appears right before the first left-aligned textblock
+                    break
+        os.remove(file_path)
+        return result
+        ### Notes: block object is a list of: (x0, y0, x1, y1, text, block_no, block_type)
+     
+     
+     
 if __name__ == '__main__':
-	#get_pdf_urls(input(r'ListOfDINs copy.xlsx'))
-	#download_pdfs()
-	df = pd.read_csv('pdf_links.csv', dtype = str)
-	df['Drug Class'] = ''
-	df = df.drop(columns='PDF Link')
-	for i in range(df.shape[0]):
-		print(df.loc[i, 'DIN'])
-		pdftoimage( df.loc[i, 'DIN'] + '.pdf' )
-		imagetoOCR( df.loc[i, 'DIN'] + '.jpg' )
-		df.loc[i , 'Drug Class'] = get_drug_class_PyMuPDF( 'OCR-' + df.loc[i, 'DIN'] + '.pdf' )
-		print('\n')
-	df.to_excel(r'List of DINS with Drug Class.xlsx')
+    # Parse CMD line arg
+    file_path = r'ListOfDINs.xlsx'
+    output_path = file_path.replace('.xlsx','') + ' with Drug Class.xlsx'
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+        
+    # Collecting product monograph pdfs
+    scrape_productMonographs_URLs(cleanExcelfile(file_path))
+    download_productMonographs()
+    
+    # Extract Drug Class from product monographs
+    df = pd.read_csv('pdf_links.csv', dtype = str)  #  
+    df['Drug Class'] = ''
+    df = df.drop(columns='PDF Link')
+    for i in range(df.shape[0]):
+        print('Extracting drug class from ' + df.loc[i, 'DIN'] + '...')
+        pdftoimage( df.loc[i, 'DIN'] + '.pdf' )
+        OCRimage( df.loc[i, 'DIN'] + '.jpg' )
+        df.loc[i , 'Drug Class'] = get_drug_class_PyMuPDF( 'OCR-' + df.loc[i, 'DIN'] + '.pdf' )
+        print('\n')
+    df.to_excel(output_path)  # Create excel sheet with extracted drug class
